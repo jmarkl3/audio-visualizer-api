90d7cecb9b787f835d0dcd1ace1a1119d29c5e8c
unknown <jmarkle@protonmail.com>
2025-08-20 17:50:36 -0400

    cors

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const app = require("express")()
+const cors = require("cors")
+
+// Enable CORS for all routes
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.json({message: "App is running"})
